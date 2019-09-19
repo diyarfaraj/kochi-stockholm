e87c0e488006aec73679e83658bb7900495699a2
Diyar Faraj <diyar.faraj@gmail.com>
2019-09-19 17:05:21 +0200

    css animate

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const nav = document.querySelector('.whole-nav');
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-    console.log('hellooo');
+    
     if(window.scrollY >= 615){
         nav.classList.add('whole-nav');
     } else {
@@ -10,9 +10,27 @@ function fixNav() {
     }
 }
 
-window.addEventListener('scroll', fixNav);
+window.addEventListener('scroll', fixNav); 
+
 
 $(document).ready(function() {
 
-    console.log("JQUERY");
+    // animate on scroll 
+    /* $('.js--wp-1').waypoint(function(direction) {
+        $('js--wp-1').addClass('animated fadeIn');
+    })
+ */
+
+    //menu fade
+    $(window).scroll(function () {
+        if ($(this).scrollDown() > 100) {
+            console.log('10000000')
+            $('.whole-nav').animate({opacity: 0.5}, 1000);   
+            } 
+        if ($(this).scrollTop() > 300) {
+            console.log('30000000')
+
+            $('.whole-nav').animate({opacity: 1}, 1000);
+            }
+     });
 })
