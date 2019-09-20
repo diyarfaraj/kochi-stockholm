b462c1a7b495518d9e7c79c58100ada28ae5ba4d
Diyar Faraj <diyar.faraj@gmail.com>
2019-09-20 07:12:23 +0200

    animation done first section

--- script.js
+++ script.js
@@ -16,13 +16,16 @@ window.addEventListener('scroll', fixNav);
 $(document).ready(function() {
 
     // animate on scroll 
-    /* $('.js--wp-1').waypoint(function(direction) {
-        $('js--wp-1').addClass('animated fadeIn');
+     $('.js--wp-1').waypoint(function(direction) {
+         console.log('helliooo')
+        $('.js--wp-1').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
     })
- */
+ 
 
     //menu fade
-    $(window).scroll(function () {
+   /*  $(window).scroll(function () {
         if ($(this).scrollDown() > 100) {
             console.log('10000000')
             $('.whole-nav').animate({opacity: 0.5}, 1000);   
@@ -33,4 +36,6 @@ $(document).ready(function() {
             $('.whole-nav').animate({opacity: 1}, 1000);
             }
      });
-})
+     */
+});
+ 
