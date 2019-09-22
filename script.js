1a8a8ac5fba60aed4dcd4170b5ac89f62d986117
Diyar Faraj <48604250+diyarfaraj@users.noreply.github.com>
2019-09-22 19:47:48 +0200

    bug fixed

--- script.js
+++ script.js
@@ -1,52 +1,56 @@
-const nav = document.querySelector('.whole-nav');
+const nav = document.querySelector(".whole-nav");
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-    
-    if(window.scrollY >= 615){
-        nav.classList.add('whole-nav');
-    } else {
-        nav.classList.remove('whole-nav');
-    }
+  if (window.scrollY >= 615) {
+    nav.classList.add("whole-nav");
+  } else {
+    nav.classList.remove("whole-nav");
+  }
 }
 
-window.addEventListener('scroll', fixNav); 
-
+window.addEventListener("scroll", fixNav);
 
 $(document).ready(function() {
+  // animate on scroll Section features
+  $(".js--wp-1").waypoint(
+    function(direction) {
+      $(".js--wp-1").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
 
-    // animate on scroll Section features
-     $('.js--wp-1').waypoint(function(direction) {
-         console.log('helliooo')
-        $('.js--wp-1').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    })
+  // animate on scroll app section
+  $(".js--wp-2").waypoint(
+    function(direction) {
+      console.log("helliooo");
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
 
-    // animate on scroll app section
-     $('.js--wp-2').waypoint(function(direction) {
-         console.log('helliooo')
-        $('.js--wp-2').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    })
-
-    // animate on scroll 
-     $('.js--wp-3').waypoint(function(direction) {
-         console.log('helliooo')
-        $('.js--wp-3').addClass('animated fadeInUp');
-    }, {
-        offset: '50%'
-    })
-    // animate on scroll 
-     $('.js--wp-4').waypoint(function(direction) {
-         console.log('helliooo')
-        $('.js--wp-4').addClass('animated pulse');
-    }, {
-        offset: '50%'
-    })
- 
-
-    
+  // animate on scroll
+  $(".js--wp-3").waypoint(
+    function(direction) {
+      console.log("helliooo");
+      $(".js--wp-3").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
+  // animate on scroll
+  $(".js--wp-4").waypoint(
+    function(direction) {
+      console.log("helliooo");
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%"
+    }
+  );
 });
- 
