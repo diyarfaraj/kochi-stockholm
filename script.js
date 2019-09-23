a4a73012e2ce88d1033c51cdb2045cb2d631cf03
Diyar Faraj <diyar.faraj@gmail.com>
2019-09-23 17:15:33 +0200

    fixing hamburger menu

--- script.js
+++ script.js
@@ -1,64 +1,71 @@
-const nav = document.querySelector(".whole-nav");
+const nav = document.querySelector('.whole-nav');
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
-  if (window.scrollY >= 615) {
-    nav.classList.add("whole-nav");
-  } else {
-    nav.classList.remove("whole-nav");
-  }
+	if (window.scrollY >= 615) {
+		nav.classList.add('whole-nav');
+	} else {
+		nav.classList.remove('whole-nav');
+	}
 }
 
-window.addEventListener("scroll", fixNav);
+window.addEventListener('scroll', fixNav);
 
 $(document).ready(function() {
-  // animate on scroll Section features
-  $(".js--wp-1").waypoint(
-    function(direction) {
-      $(".js--wp-1").addClass("animated fadeInUp");
-    },
-    {
-      offset: "50%"
-    }
-  );
+	// animate on scroll Section features
+	$('.js--wp-1').waypoint(
+		function(direction) {
+			$('.js--wp-1').addClass('animated fadeInUp');
+		},
+		{
+			offset: '50%'
+		}
+	);
 
-  // animate on scroll app section
-  $(".js--wp-2").waypoint(
-    function(direction) {
-      console.log("helliooo");
-      $(".js--wp-2").addClass("animated fadeInUp");
-    },
-    {
-      offset: "50%"
-    }
-  );
+	// animate on scroll app section
+	$('.js--wp-2').waypoint(
+		function(direction) {
+			console.log('helliooo');
+			$('.js--wp-2').addClass('animated fadeInUp');
+		},
+		{
+			offset: '50%'
+		}
+	);
 
-  // animate on scroll
-  $(".js--wp-3").waypoint(
-    function(direction) {
-      console.log("helliooo");
-      $(".js--wp-3").addClass("animated fadeInUp");
-    },
-    {
-      offset: "50%"
-    }
-  );
-  // animate on scroll
-  $(".js--wp-4").waypoint(
-    function(direction) {
-      console.log("helliooo");
-      $(".js--wp-4").addClass("animated pulse");
-    },
-    {
-      offset: "50%"
-    }
-  );
+	// animate on scroll
+	$('.js--wp-3').waypoint(
+		function(direction) {
+			console.log('helliooo');
+			$('.js--wp-3').addClass('animated fadeInUp');
+		},
+		{
+			offset: '50%'
+		}
+	);
+	// animate on scroll
+	$('.js--wp-4').waypoint(
+		function(direction) {
+			console.log('helliooo');
+			$('.js--wp-4').addClass('animated pulse');
+		},
+		{
+			offset: '50%'
+		}
+	);
 
-  /* MObile navigation */
+	/* MObile navigation */
 
-  $('.js--nav-icon').click(function() {
-    $('.main-nav').slideToggle(500);
-    //add   <ion-icon name="close"></ion-icon>
+	$('.js--nav-icon').click(function() {
+		var nav = $('.js--main-nav');
+		var icon = $('.js--nav-icon');
+		nav.slideToggle(500);
+		//add   <ion-icon name="close"></ion-icon>
 
-  })
+		if (icon.hasClass('hamburger')) {
+			icon.addClass('cross');
+		} else {
+			icon.addClass('hamburger');
+		}
+	});
 });
